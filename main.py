5e3c274de0504f1e6cdeeb5b39d302a9e644ac63
Leon McClure <ggawta@mail.ru>
2025-10-16 00:17:33 +0800

    Добавлено приветствие v2

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
+import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
-
 token = os.getenv('TELEGRAM_TOKEN')
 
-print(token)
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    hi_text = "Привет! Я бот для скачивания медиа. Отправьте мне фото, видео или ссылку на стикерпак."
+    bot.reply_to(message, hi_text)
+
+bot.polling(none_stop=True)
