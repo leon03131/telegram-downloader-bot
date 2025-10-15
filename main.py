5e96d598fc2c66a67029e6d8ab1e48facbcbefa8
Leon McClure <ggawta@mail.ru>
2025-10-16 00:25:15 +0800

    Ловитель фото

--- main.py
+++ main.py
@@ -12,4 +12,9 @@ def handle_start(message):
     hi_text = "Привет! Я бот для скачивания медиа. Отправьте мне фото, видео или ссылку на стикерпак."
     bot.reply_to(message, hi_text)
 
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    ph_text = "Я получил фото! 👌🏿"
+    bot.reply_to(message, ph_text)
+
 bot.polling(none_stop=True)
